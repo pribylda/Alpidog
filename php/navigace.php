455e1eba67c3a0e83f57857ff1c428c0d8bbf0bc
Davča <davidpribyl09@gmail.com>
2025-05-14 14:24:53 +0200

    Add files via upload

---
+++ php/navigace.php
@@ -0,0 +1,26 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "Instagram",
+      "item": "https://alpidogmako.cz/instagram/"
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "name": "Štěňata",
+      "item": "https://alpidogmako.cz/stenata/"
+    },
+    {
+      "@type": "ListItem",
+      "position": 3,
+      "name": "Kontakt",
+      "item": "https://alpidogmako.cz/kontakt/"
+    }
+  ]
+}
+</script>
