5dbc6a5be01553904177873303bfb3b4c9942d0b
Davča <davidpribyl09@gmail.com>
2025-05-16 13:25:57 +0200

    Delete javascript directory

--- javascript/navbar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-      navbar.classList.add("shrunk");
-    } else {
-      navbar.classList.remove("shrunk");
-    }
-  });
-  
